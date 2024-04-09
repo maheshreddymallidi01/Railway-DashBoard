5e2d4f51bdae46da5898ea7352c82ed21fb6da03
Mahesh <maheshreddymallidi01@gmail.com>
2024-04-09 19:22:58 +0530

    Updated Controller

--- src/main/java/com/tbp/crud/controller/TrainController.java
+++ src/main/java/com/tbp/crud/controller/TrainController.java
@@ -1,11 +1,21 @@
 package com.tbp.crud.controller;
 
-import com.tbp.crud.dao.TrainRepository;
-import com.tbp.crud.entity.Train;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tbp.crud.dao.TrainRepository;
+import com.tbp.crud.entity.Train;
 
 @RestController
 public class TrainController {
@@ -13,32 +23,32 @@ public class TrainController {
     TrainRepository trainRepository;
 
     @PostMapping("/train")
-    public Train addTrain(@RequestBody Train train) {
+    public ResponseEntity<Train> addTrain(@RequestBody Train train) {
 
-        return trainRepository.saveTrain(train);
+        return new ResponseEntity<>(trainRepository.saveTrain(train),HttpStatus.CREATED);
 
     }
 
     @PutMapping("/train")
-    public Train updateTrain(@RequestBody Train train) {
+    public ResponseEntity<Train> updateTrain(@RequestBody Train train) {
 
-        return trainRepository.updateTrain(train);
+        return new ResponseEntity<>(trainRepository.updateTrain(train),HttpStatus.ACCEPTED);
 
     }
 
     @GetMapping("/train/{id}")
-    public Train getTrain(@PathVariable("id") int id) {
-        return trainRepository.getById(id);
+    public ResponseEntity<Train> getTrain(@PathVariable("id") int id) {
+        return new ResponseEntity<>(trainRepository.getById(id),HttpStatus.OK);
     }
 
     @GetMapping("/trains")
-    public List<Train> getTrains() {
-        return trainRepository.allTrains();
+    public ResponseEntity<List<Train>> getTrains() {
+        return new ResponseEntity<>(trainRepository.allTrains(),HttpStatus.OK);
     }
 
     @DeleteMapping("/train/{id}")
-    public String deleteTrain(@PathVariable("id") int id){
-        return trainRepository.deleteById(id);
+    public ResponseEntity<String> deleteTrain(@PathVariable("id") int id){
+        return new ResponseEntity<>(trainRepository.deleteById(id),HttpStatus.OK);
     }
 
 }
